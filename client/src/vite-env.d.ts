bee58cf316ae92805c7aec941b1f6caf0095590a
GeetEditz <geet.more15@gmail.com>
2025-05-17 10:50:28 +0530

    API

---
+++ client/src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+
+interface ImportMeta {
+    readonly env: {
+      readonly VITE_API_URL: "https://code-editor-server-production.up.railway.app";
+      // Add other environment variables as needed
+    };
+  }
